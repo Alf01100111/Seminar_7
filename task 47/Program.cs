e548ac98b834db1e5d2c3c2bcd45e5ea91728608
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-17 22:15:09 +0300

    first task

---
+++ task 47/Program.cs
@@ -0,0 +1,28 @@
+﻿double[,] GetRndMatrix(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            double rnd = new Random().Next(min, max);
+            matrix[i,j] = rnd/10;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");            
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] newmatrix = GetRndMatrix(5,4,-100,100);
+PrintMatrix(newmatrix);
