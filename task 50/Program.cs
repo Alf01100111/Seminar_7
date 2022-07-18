c7206637292baa9e22607370fedf175e6deb43a9
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-18 21:41:32 +0300

    task 50;

--- task 50/Program.cs
+++ task 50/Program.cs
@@ -6,7 +6,7 @@
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
             double rnd = new Random().Next(min, max);
-            matrix[i,j] = rnd;
+            matrix[i, j] = rnd;
         }
     }
     return matrix;
@@ -18,8 +18,29 @@ void PrintMatrix(double[,] matrix)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{matrix[i,j]}\t");            
+            Console.Write($"{matrix[i, j]}\t");
         }
         Console.WriteLine();
     }
 }
+
+void ShowNumInPos(double[,] matrix, int rows, int columns)
+{
+    if (rows > matrix.GetLength(0) || columns > matrix.GetLength(1) || rows < 0 || columns < 0)
+    {
+        System.Console.WriteLine("Такой позиции нет в данной матрице ");   // Спросить на семинаре как сделать через ловлю ошибки System.IndexOutOfRangeException
+    }    
+    System.Console.WriteLine($"На позиции строка: {rows+1} столбец: {columns+1} - число {matrix[rows, columns]}");
+}
+
+double[,] matrix = GetRndMatrix(5, 4, 0, 100);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+
+System.Console.Write("Ведите строку - ");
+int row = Convert.ToInt32(Console.ReadLine()) - 1;
+
+System.Console.Write("Ведите столбец - ");
+int column = Convert.ToInt32(Console.ReadLine()) - 1;
+
+ShowNumInPos(matrix, row, column);
---
+++ task 52/Program.cs
@@ -0,0 +1,45 @@
+﻿int[,] GetRndMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = GetRndMatrix(5, 4, 0, 10);
+PrintMatrix(matrix);
+
+int[] AverInColumns(int[,] matrix)
+{
+    int[] resultar = new int[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)  //пробежимся по каждому столбцу матрицы
+        {                                              //получим массив с суммами каждого столбца
+            resultar[j] += matrix[i, j];
+        }
+    }
+    for (int i = 0; i < resultar.Length; i++)
+    {
+        resultar[i] = resultar[i] / matrix.GetLength(0);  //считаем среднее для каждого элемента массива
+    }
+    return resultar;
+}
+
